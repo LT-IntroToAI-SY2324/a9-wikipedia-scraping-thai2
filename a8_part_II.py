39ecc50af56f5840ca771db5bcd60fb0702a9aaa
Eric <ethai1@cps.edu>
2024-05-13 11:31:15 -0500

    worked on part II

--- a8_part_II.py
+++ a8_part_II.py
@@ -20,7 +20,10 @@ def get_planet_radius(planet_name: str) -> str:
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
     # TODO: fill this in
-    pattern = "REPLACE ME"
+
+    
+
+    pattern = "IDK YET"
     error_text = "Page infobox has no polar radius information"
     match = get_match(infobox_text, pattern, error_text)
     return match.group("radius")
@@ -44,6 +47,8 @@ def get_birth_date(name: str) -> str:
     match = get_match(infobox_text, pattern, error_text)
     return match.group("birth")
 
+# infobox_text = get_first_infobox_text(get_page_html("Mars"))
+# print(infobox_text)
 
 if __name__ == "__main__":
     print("\n<<<<<<<<<<<<<< Testing Planet Radius >>>>>>>>>>>>>>")
